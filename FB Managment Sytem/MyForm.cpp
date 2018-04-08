6c5c5f87801cbdd14cc4b57a11ec20d8c55fabbb
HassanAdham <32961869+HassanAdham@users.noreply.github.com>
2018-04-08 23:13:06 +0200

    First Pro

---
+++ FB Managment Sytem/MyForm.cpp
@@ -0,0 +1,20 @@
+#include "MyForm.h"
+
+using namespace System;
+using namespace System::ComponentModel;
+using namespace System::Collections;
+using namespace System::Windows::Forms;
+using namespace System::Data;
+using namespace System::Drawing;
+
+[STAThreadAttribute]
+int main(array<System::String ^> ^args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	ConsoleApplication1::asdasdad mainForm;
+	Application::Run(%mainForm);
+
+    return 0;
+}
+
